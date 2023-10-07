643d02efec0869e1dd502ca85792a6d249dec453
Pallagi Joe <pallagijoe@gmail.com>
2023-10-07 15:36:39 +0200

    code code academy contact form

--- src/app/contact-form/pages/contact-form-page/contact-form-page.component.ts
+++ src/app/contact-form/pages/contact-form-page/contact-form-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { Store } from '@ngrx/store';
 import { combineLatest, filter, tap } from 'rxjs';
@@ -17,25 +18,49 @@ import { FormRequestInterface } from '../../types/form-request.interface';
     imports: [IonicModule, ReactiveFormsModule, CommonModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactFormPageComponent implements OnInit {
-    constructor(private formBuilder: FormBuilder, private store: Store, private dialogConfirm: DialogConfirmService) {}
+export class ContactFormPageComponent {
 
-    data$ = combineLatest({
+    public currentMentoringType: string = '';
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private store: Store,
+        private dialogConfirm: DialogConfirmService,
+        private activatedRoute: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    public data$ = combineLatest({
         isSubmitting: this.store.select(selectIsSubmitting),
         onConfirm: this.dialogConfirm.onConfirmAction$.pipe(
             filter(Boolean),
             tap(() => this.clearForm())
         ),
+        paramHandling : this.activatedRoute.queryParamMap.pipe(
+            tap((params) => {
+                const mentoringType = params.get('mentoringType');
+                this.currentMentoringType = mentoringType || '';
+                if (mentoringType) {
+                    this.form.patchValue({ subject: mentoringType });
+                }
+            })
+        )
     });
 
-    ngOnInit() {}
     public form = this.formBuilder.nonNullable.group({
         firstName: ['', Validators.required],
         lastName: ['', Validators.required],
         email: ['', [Validators.email, Validators.required]],
         phoneNumber: ['', [Validators.pattern(/^\+?[0-9]{10,}$/)]],
-        message: ['', Validators.required],
+        subject: ['', [Validators.required]],
+        message: [''],
     });
+
+
+
+    public handleChange(event: any) {
+        this.router.navigate([], { queryParams: { mentoringType: event.detail.value }, queryParamsHandling: 'merge' });
+    }
 
     public onSubmit() {
         const request: FormRequestInterface = this.form.getRawValue();
